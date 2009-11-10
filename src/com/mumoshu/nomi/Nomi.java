e141520804d536ba421868b1163d3eff53822401
mumoshu <ykuoka.gmail.com>
2009-11-11 01:57:06 +0900

    it now finds nearby restaurants, bars, etc. automatically.

--- src/com/mumoshu/nomi/Nomi.java
+++ src/com/mumoshu/nomi/Nomi.java
@@ -1,8 +1,19 @@
 package com.mumoshu.nomi;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.location.Address;
@@ -10,8 +21,10 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.google.android.maps.GeoPoint;
@@ -22,7 +35,7 @@ import com.google.android.maps.OverlayItem;
 import com.mumoshu.maps.MarkerPane;
 
 public class Nomi extends MapActivity implements LocationListener {
-	static final int INITIAL_ZOOM_LEVEL = 15;
+	static final int INITIAL_ZOOM_LEVEL = 11;
 	static final int INITIAL_LATITUDE = 35455281;
 	static final int INITIAL_LONGITUDE = 139629711;
 	
@@ -42,6 +55,7 @@ public class Nomi extends MapActivity implements LocationListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.main);
         
         this.initialPoint = new GeoPoint(Nomi.INITIAL_LATITUDE, Nomi.INITIAL_LONGITUDE);
@@ -52,6 +66,7 @@ public class Nomi extends MapActivity implements LocationListener {
         this.mapController.setCenter(this.initialPoint);
         this.mapController.setZoom(this.initialZoom);
         
+        // TODO split markerPane for managing the center marker and the other markers independently.
         this.markerPane = new MarkerPane(this.getResources().getDrawable(R.drawable.androidmarker));
         this.mapView.getOverlays().add(this.markerPane);
         this.markerPane.addOverlay(new OverlayItem(this.initialPoint, "title", "snippet"));
@@ -98,9 +113,16 @@ public class Nomi extends MapActivity implements LocationListener {
 				(int)(location.getLongitude() * 1E6));
 		this.mapController.animateTo(point);
 		this.showLocation(location);
+		String url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=4e134a6779786c91";
+		url += "&lat=" + String.valueOf(location.getLatitude());
+		url += "&lng=" + String.valueOf(location.getLongitude());
+		url += "&range=5&order=41&format=json";
+		new HttpGetTask().execute(url);
+		this.markerPane.addOverlay(new OverlayItem(point, "title", "snippet"));
 	}
 
 	private void showLocation(Location location) {
+		setProgressBarIndeterminateVisibility(true); 
 		double lat = location.getLatitude();
 		double lng = location.getLongitude();
 		StringBuffer buf = new StringBuffer();
@@ -136,5 +158,92 @@ public class Nomi extends MapActivity implements LocationListener {
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void showSpots(String jsonString) {
+		JSONObject json = null;
+		JSONObject results = null;
+		JSONArray shopsJSON;
+		ArrayList<JSONObject> shops = new ArrayList<JSONObject>();
+		try {
+			json = new JSONObject(jsonString);
+			results = json.getJSONObject("results");
+			shopsJSON = results.getJSONArray("shop");
+			Log.i("Shops",String.valueOf(shopsJSON.length()));
+			for(int i=0; i< shopsJSON.length(); i++) {
+				JSONObject shop = shopsJSON.getJSONObject(i);
+				shops.add(shop);
+				GeoPoint point = new GeoPoint((int)(shop.getDouble("lat") * 1E6), (int)(shop.getDouble("lng") * 1E6));
+				this.markerPane.addOverlay(new OverlayItem(point, shop.getString("name"), shop.getString("catch")));
+				Log.i("shop point", point.toString());
+			}
+			
+		} catch (Exception e) {
+			Log.e(this.getClass().toString(), e.getMessage());
+			e.printStackTrace();
+			Log.e("results:", results.toString());
+			return;
+		}
+		setProgressBarIndeterminateVisibility(false); 
+	}
+			
+	public class HttpGetTask extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... urls) {
+			
+			DefaultHttpClient http = new DefaultHttpClient(); 
+			HttpGet get = new HttpGet();
+			HttpResponse res;
+			URI uri;
+			
+			try {
+			 uri = new URI(urls[0]);
+			} catch( URISyntaxException e) {
+				Log.e(this.getClass().toString(), e.getMessage());
+				e.printStackTrace();
+				return null;
+			}
+			
+			try {
+				get.setURI(uri);
+				res = http.execute(get);
+				int status = res.getStatusLine().getStatusCode();
+				if (200 <= status && status <= 304) {
+					BufferedReader stream = new BufferedReader(new InputStreamReader(res.getEntity().getContent())); 
+					StringBuffer strBuf = new StringBuffer();
+					try {
+						String line;
+						while ((line = stream.readLine()) != null) {
+							strBuf.append(line);
+						}
+					} catch (Exception e) {
+						Log.e(this.getClass().toString(), e.getMessage());
+						e.printStackTrace();
+						return null;
+					} finally {
+						try {
+							stream.close();
+						} catch (Exception e) {
+							Log.e(this.getClass().toString(), e.getMessage());
+							e.printStackTrace();
+						}
+					}
+					return strBuf.toString(); 
+				}
+			} catch (Exception e) {
+				Log.e(this.getClass().toString(), e.getMessage());
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			showSpots(result);
+			
+		}
+
 	}
 }
